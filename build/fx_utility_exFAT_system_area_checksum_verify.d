c2f2a529bcef27b6cec2b843bf4fc463ef6d1c9c
sai <sai@cloud2future.com>
2024-07-21 12:24:54 -0700

    Initial Push

---
+++ build/fx_utility_exFAT_system_area_checksum_verify.d
@@ -0,0 +1,13 @@
+build/fx_utility_exFAT_system_area_checksum_verify.o: \
+ Middlewares/ST/filex/common/src/fx_utility_exFAT_system_area_checksum_verify.c \
+ Middlewares/ST/filex/common/inc/fx_api.h \
+ Middlewares/ST/filex/ports/generic/inc/fx_port.h FileX/App/fx_user.h \
+ Middlewares/ST/threadx/common/inc/tx_api.h \
+ Middlewares/ST/threadx/ports/cortex_m4/gnu/inc/tx_port.h \
+ Core/Inc/tx_user.h
+Middlewares/ST/filex/common/inc/fx_api.h:
+Middlewares/ST/filex/ports/generic/inc/fx_port.h:
+FileX/App/fx_user.h:
+Middlewares/ST/threadx/common/inc/tx_api.h:
+Middlewares/ST/threadx/ports/cortex_m4/gnu/inc/tx_port.h:
+Core/Inc/tx_user.h:
---
+++ build/ux_hcd_stm32_request_control_transfer.d
@@ -0,0 +1,82 @@
+build/ux_hcd_stm32_request_control_transfer.o: \
+ Middlewares/ST/usbx/common/usbx_stm32_host_controllers/ux_hcd_stm32_request_control_transfer.c \
+ Middlewares/ST/usbx/common/core/inc/ux_api.h \
+ Middlewares/ST/usbx/ports/generic/inc/ux_port.h USBX/App/ux_user.h \
+ Middlewares/ST/threadx/common/inc/tx_api.h \
+ Middlewares/ST/threadx/ports/cortex_m4/gnu/inc/tx_port.h \
+ Core/Inc/tx_user.h Middlewares/ST/usbx/common/core/inc/ux_utility.h \
+ Middlewares/ST/usbx/common/core/inc/ux_system.h \
+ Middlewares/ST/usbx/common/usbx_stm32_host_controllers/ux_hcd_stm32.h \
+ USBX/Target/ux_stm32_config.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ Core/Inc/stm32l4xx_hal_conf.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4r5xx.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/mpu_armv7.h \
+ Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_hcd.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_usb.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ Middlewares/ST/usbx/common/core/inc/ux_host_stack.h
+Middlewares/ST/usbx/common/core/inc/ux_api.h:
+Middlewares/ST/usbx/ports/generic/inc/ux_port.h:
+USBX/App/ux_user.h:
+Middlewares/ST/threadx/common/inc/tx_api.h:
+Middlewares/ST/threadx/ports/cortex_m4/gnu/inc/tx_port.h:
+Core/Inc/tx_user.h:
+Middlewares/ST/usbx/common/core/inc/ux_utility.h:
+Middlewares/ST/usbx/common/core/inc/ux_system.h:
+Middlewares/ST/usbx/common/usbx_stm32_host_controllers/ux_hcd_stm32.h:
+USBX/Target/ux_stm32_config.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+Core/Inc/stm32l4xx_hal_conf.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4r5xx.h:
+Drivers/CMSIS/Include/core_cm4.h:
+Drivers/CMSIS/Include/cmsis_version.h:
+Drivers/CMSIS/Include/cmsis_compiler.h:
+Drivers/CMSIS/Include/cmsis_gcc.h:
+Drivers/CMSIS/Include/mpu_armv7.h:
+Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_hcd.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_usb.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
+Middlewares/ST/usbx/common/core/inc/ux_host_stack.h:
